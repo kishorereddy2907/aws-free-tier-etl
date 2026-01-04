ff70c2acc8ec53dcc13d6722647180bccf305cfd
kishorereddy2907 <kishore560037@gmail.com>
2026-01-04 15:56:47 +0530

    feat: Implement S3 CSV to partitioned Parquet ETL with deduplication.

--- scripts/glue_job.py
+++ scripts/glue_job.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder.getOrCreate()
 df = spark.read.option("header", "true") \
     .csv("s3://aws-free-tier-etl-raw-446072489762/employees_global/")
 
-df_dedup = df.dropDuplicates(["email"])
+df_dedup = df.dropDuplicates(["email"], ["country"])
 
 df_dedup.write.mode("overwrite") \
     .partitionBy("country") \
